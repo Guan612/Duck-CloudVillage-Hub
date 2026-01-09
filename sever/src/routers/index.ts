2c5058c2bda569e9b6bb8bda9431f24d037943c7
Guan612 <tiantony53@163.com>
2026-01-09 17:14:56 +0800

    导入产品api

--- sever/src/routers/index.ts
+++ sever/src/routers/index.ts
@@ -1,10 +1,12 @@
 import { Hono } from "hono";
 import authRouter from "./auth";
 import userRouter from "./users";
+import productsRotter from "./products";
 
-const api = new Hono()
+const api = new Hono();
 
 api.route("/auth", authRouter);
 api.route("/user", userRouter);
+api.route("/product", productsRotter);
 
-export default api
+export default api;
---
+++ sever/src/routers/products.ts
@@ -0,0 +1,40 @@
+import { Hono } from "hono";
+import { jwt } from "hono/jwt";
+import { appConfig } from "../config";
+import { db } from "../db";
+import { eq } from "drizzle-orm";
+import { products } from "../db/schema";
+import { insertProductSchema } from "../validators";
+import z from "zod";
+
+const productsRotter = new Hono();
+
+productsRotter.use(
+  jwt({
+    secret: appConfig.jwt.secret,
+  }),
+);
+
+productsRotter.get("/", async (c) => {
+  const res = await db.query.products.findMany();
+  return c.json(res);
+});
+
+productsRotter.get("/:id", async (c) => {
+  const productId = c.req.param("id");
+  const res = await db.query.products.findFirst({
+    where: eq(products.id, Number(productId)),
+  });
+  return c.json(res);
+});
+
+productsRotter.post("/", async (c) => {
+  const body = await c.req.json();
+  const req = insertProductSchema.safeParse(body);
+  if (!req.success) {
+    return c.json({ error: z.flattenError(req.error) }, 400);
+  }
+
+  
+
+});
