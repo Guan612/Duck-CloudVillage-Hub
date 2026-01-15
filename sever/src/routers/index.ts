29b6e883cebf6d32778c2518e651653dfd41e68f
Guan612 <tiantony53@163.com>
2026-01-15 08:21:53 +0800

    新增测试路由

--- sever/src/routers/index.ts
+++ sever/src/routers/index.ts
@@ -2,11 +2,13 @@ import { Hono } from "hono";
 import authRouter from "./auth";
 import userRouter from "./users";
 import productsRotter from "./products";
+import tests from "./tests";
 
 const api = new Hono();
 
 api.route("/auth", authRouter);
 api.route("/user", userRouter);
 api.route("/product", productsRotter);
+api.route("/test", tests);
 
 export default api;
