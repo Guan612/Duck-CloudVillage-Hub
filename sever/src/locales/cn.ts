db4a354d1af70a5d052d3a1e412770fa5a927bcf
Guan612 <tiantony53@163.com>
2026-01-15 10:41:49 +0800

    翻译文件

---
+++ sever/src/locales/cn.ts
@@ -0,0 +1,14 @@
+export default {
+  welcome: "欢迎你，{name}",
+  success: "操作成功",
+  fail: "操作失败",
+  param_error: "参数验证错误",
+  errors: {
+    not_found: "找不到资源",
+  },
+  auth: {
+    login_err: "用户名或密码错误",
+    login_success: "登录成功",
+    user_exit: "用户已存在，请登录",
+  },
+};
