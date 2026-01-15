550d98c0568c90599cc9ddf5509f7a8efce9ce01
Guan612 <tiantony53@163.com>
2026-01-15 08:14:54 +0800

    添加多语言路由

--- sever/src/index.ts
+++ sever/src/index.ts
@@ -1,10 +1,20 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { cors } from "hono/cors";
+import { languageDetector } from "hono/language";
 import api from "./routers";
 
 const app = new Hono();
 
-app.use("*", cors()).use(logger()).route("/api", api);
+app
+  .use("*", cors())
+  .use(logger())
+  .use(
+    languageDetector({
+      supportedLanguages: ["cn", "bo"], // Must include fallback
+      fallbackLanguage: "cn", // Required
+    }),
+  )
+  .route("/api", api);
 
 export default app;
