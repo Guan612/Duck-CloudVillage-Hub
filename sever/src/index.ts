26d2961a03fbb3a086fce04f5cb1ec4c1055ed6b
Guan612 <tiantony53@163.com>
2026-01-15 10:41:30 +0800

    修改多语言

--- sever/src/index.ts
+++ sever/src/index.ts
@@ -1,20 +1,11 @@
 import { Hono } from "hono";
 import { logger } from "hono/logger";
 import { cors } from "hono/cors";
-import { languageDetector } from "hono/language";
 import api from "./routers";
+import { i18n } from "./middleware/i18n";
 
 const app = new Hono();
 
-app
-  .use("*", cors())
-  .use(logger())
-  .use(
-    languageDetector({
-      supportedLanguages: ["cn", "bo"], // Must include fallback
-      fallbackLanguage: "cn", // Required
-    }),
-  )
-  .route("/api", api);
+app.use("*", cors()).use(logger()).use("*", i18n).route("/api", api);
 
 export default app;
