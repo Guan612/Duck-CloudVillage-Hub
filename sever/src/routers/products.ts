37a28e26c9ec70d026d0dcfcabac78b4c687f0dd
Guan612 <tiantony53@163.com>
2026-01-15 15:32:06 +0800

    新增产品api

--- sever/src/routers/products.ts
+++ sever/src/routers/products.ts
@@ -4,8 +4,10 @@ import { appConfig } from "../config";
 import { db } from "../db";
 import { eq } from "drizzle-orm";
 import { products } from "../db/schema";
-import { insertProductSchema } from "../validators";
+import { insertProductSchema, updateProductSchema } from "../validators";
 import z from "zod";
+import { fail, success } from "../utils/result";
+import { useTranslation } from "@intlify/hono";
 
 const productsRotter = new Hono();
 
@@ -17,7 +19,7 @@ productsRotter.use(
 
 productsRotter.get("/", async (c) => {
   const res = await db.query.products.findMany();
-  return c.json(res);
+  return c.json(success(res));
 });
 
 productsRotter.get("/:id", async (c) => {
@@ -25,19 +27,35 @@ productsRotter.get("/:id", async (c) => {
   const res = await db.query.products.findFirst({
     where: eq(products.id, Number(productId)),
   });
-  return c.json(res);
+  return c.json(success(res));
 });
 
 productsRotter.post("/", async (c) => {
   const body = await c.req.json();
+  const t = await useTranslation(c);
   const req = insertProductSchema.safeParse(body);
   if (!req.success) {
-    return c.json({ error: z.flattenError(req.error) }, 400);
+    return c.json(fail(t("param_error"), z.flattenError(req.error)), 400);
   }
+  const res = await db.insert(products).values(req.data);
 
-
-
+  return c.json(success(res));
 });
 
+productsRotter.patch("/:id", async (c) => {
+  const productId = c.req.param("id");
+  const t = await useTranslation(c);
+  const body = await c.req.json();
+  const req = updateProductSchema.safeParse(body);
+  if (!req.success) {
+    return c.json(fail(t("param_error"), z.flattenError(req.error)), 400);
+  }
+  const res = await db
+    .update(products)
+    .set(req.data)
+    .where(eq(products.id, Number(productId)));
 
-export default productsRotter
+  return c.json(success(res));
+});
+
+export default productsRotter;
