1ba273067be396091d1a181a20dfc0ffa634ade8
Guan612 <tiantony53@163.com>
2026-01-16 12:39:17 +0800

    用户注册登录api

---
+++ cilent/src/http/auth.ts
@@ -0,0 +1,9 @@
+import { http } from ".";
+
+export const loginApi = (data) => {
+  return http.post("/auth/login", data);
+};
+
+export const registerApi = (data) => {
+  return http.post("auth/register", data);
+};
