a1b351a20fc8b704490b644fe2b532f4ee074525
Guan612 <tiantony53@163.com>
2026-01-16 12:41:23 +0800

    全局提示

--- cilent/src/routes/__root.tsx
+++ cilent/src/routes/__root.tsx
@@ -1,10 +1,12 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { Toaster } from "sonner";
 
 const RootLayout = () => (
   <>
     <Outlet />
     <TanStackRouterDevtools />
+    <Toaster position="top-center" richColors />
   </>
 );
 
