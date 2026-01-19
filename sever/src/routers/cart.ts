4c2a9f21901073cd7a63f502e8a230e2ad6fe912
Guan612 <tiantony53@163.com>
2026-01-19 21:02:41 +0800

    完善购物车

--- sever/src/routers/cart.ts
+++ sever/src/routers/cart.ts
@@ -1,11 +1,12 @@
 import { createRoute, OpenAPIHono } from "@hono/zod-openapi";
 import { db } from "../db";
 import { eq } from "drizzle-orm";
-import { success } from "../utils/result";
+import { fail, success } from "../utils/result";
 import { jwt } from "hono/jwt";
 import { appConfig } from "../config";
 import { insertCartSchema, updateCartSchema } from "../validators";
 import { carts } from "../db/schema";
+import { useTranslation } from "@intlify/hono";
 
 const cartRoter = new OpenAPIHono();
 
@@ -15,7 +16,7 @@ cartRoter.use(
   }),
 );
 
-const listUserCart = createRoute({
+const listUserCartRouter = createRoute({
   summary: "获取用户购物车",
   security: [{ Bearer: [] }],
   method: "get",
@@ -25,7 +26,7 @@ const listUserCart = createRoute({
   },
 });
 
-cartRoter.openapi(listUserCart, async (c) => {
+cartRoter.openapi(listUserCartRouter, async (c) => {
   const payload = c.get("jwtPayload");
   const userId = payload.userId;
 
@@ -35,7 +36,7 @@ cartRoter.openapi(listUserCart, async (c) => {
   return c.json(success(res));
 });
 
-const insertUserCart = createRoute({
+const insertUserCartRouter = createRoute({
   summary: "添加购物车",
   security: [{ Bearer: [] }],
   method: "post",
@@ -50,7 +51,7 @@ const insertUserCart = createRoute({
   },
 });
 
-cartRoter.openapi(insertUserCart, async (c) => {
+cartRoter.openapi(insertUserCartRouter, async (c) => {
   const payload = c.get("jwtPayload");
   const userId = payload.userId;
   const data = c.req.valid("json");
@@ -58,32 +59,37 @@ cartRoter.openapi(insertUserCart, async (c) => {
   return c.json(success(res));
 });
 
-const updateUserCart = createRoute({
+const updateUserCartRouter = createRoute({
   summary: "修改购物车数据",
   security: [{ Bearer: [] }],
   method: "post",
-  path: "/",
+  path: "/{id}",
   request: {
     body: {
       content: { "application/json": { schema: updateCartSchema } },
     },
   },
   responses: {
-    200: { description: "成功获取购物车数据" },
+    200: { description: "成功修改购物车数据" },
+    404: { description: "没有找到购物车数据" },
   },
 });
 
-cartRoter.openapi(updateUserCart, async (c) => {
+cartRoter.openapi(updateUserCartRouter, async (c) => {
   const payload = c.get("jwtPayload");
   const userId = payload.userId;
+  const cartId = c.req.param("id");
   const data = c.req.valid("json");
 
-  const res = await db.update(carts).set(data).where(eq(carts.userId, userId));
+  const res = await db
+    .update(carts)
+    .set(data)
+    .where(eq(carts.userId, userId) && eq(carts.id, Number(cartId)));
 
   return c.json(success(res));
 });
 
-const deletUserCart = createRoute({
+const deletUserCartRouter = createRoute({
   summary: "删除购物车数据",
   security: [{ Bearer: [] }],
   method: "delete",
@@ -91,6 +97,24 @@ const deletUserCart = createRoute({
   responses: {
     200: { description: "成功删除" },
   },
+});
+
+cartRoter.openapi(deletUserCartRouter, async (c) => {
+  const t = await useTranslation(c);
+  const payload = c.get("jwtPayload");
+  const userId = payload.userId;
+  const cartId = c.req.param("id");
+
+  const res = await db
+    .delete(carts)
+    .where(eq(carts.id, Number(cartId)) && eq(userId, Number(userId)))
+    .returning;
+
+  if (!res) {
+    return c.json(fail(t("param_error")), 404);
+  }
+
+  return c.json(success(res));
 });
 
 export default cartRoter;
