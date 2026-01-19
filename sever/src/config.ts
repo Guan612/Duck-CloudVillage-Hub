9b6bab03f585aee7e069ce7b2373cbdb78d58265
Guan612 <tiantony53@163.com>
2026-01-19 09:30:27 +0800

    双token准备

--- sever/src/config.ts
+++ sever/src/config.ts
@@ -1,8 +1,11 @@
 export const appConfig = {
   jwt: {
-    secret: process.env.JWT_SECRET || "default_secret_dont_use_in_prod",
+    access_secret:
+      process.env.JWT_ACCESS_SECRET || "default_secret_dont_use_in_prod",
+    refresh_secret: process.env.JWT_REFRESH_SECRET || "default_secret_d",
     // 统一在这里处理类型转换
-    expiresIn: Number(process.env.JWT_EXPIRES_IN || 604800),
+    access_expiresIn: Number(process.env.JWT_ACCESS_EXPIRES_IN || 120000), // 2小时
+    refresh_expiresIn: Number(process.env.JWT_REFRESH_EXPIRES_IN || 604800),
   },
   server: {
     port: Number(process.env.PORT || 3000),
