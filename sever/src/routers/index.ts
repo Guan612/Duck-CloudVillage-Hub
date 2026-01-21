9de08ea265f51447ab358115397a2bbdbd43db23
Guan612 <tiantony53@163.com>
2026-01-21 22:02:40 +0800

    新增路由

--- sever/src/routers/index.ts
+++ sever/src/routers/index.ts
@@ -4,6 +4,8 @@ import userRouter from "./users";
 import productsRotter from "./products";
 import tests from "./tests";
 import cartRoter from "./cart";
+import ordersRouter from "./orders";
+import feedbacksRouter from "./feedbacks";
 
 const api = new OpenAPIHono();
 
@@ -11,6 +13,8 @@ api.route("/auth", authRouter);
 api.route("/user", userRouter);
 api.route("/product", productsRotter);
 api.route("/cart", cartRoter);
+api.route("/order", ordersRouter);
+api.route("/feedback", feedbacksRouter);
 api.route("/test", tests);
 
 export default api;
